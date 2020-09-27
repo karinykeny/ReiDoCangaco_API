4228ff00f581b3fbddfe54a09b7776589d2b3e2d
kkc-at-002407643782 <kariny.dantas@hivecloud.com.br>
2020-09-27 20:31:17 -0300

    Estrutura basica das classes e crud pedido

---
+++ models/vendedor.py
@@ -0,0 +1,7 @@
+class VendedorModel:
+    def __init__(self, cod_vendedor, nome_vendedor, login, senha, ativo):
+        self.cod_vendedor = cod_vendedor
+        self.nome_vendedor = nome_vendedor
+        self.login = login
+        self.senha = senha
+        self.ativo = ativo
