32844afa6b1249e7b02ad72868db3c44e6acc72c
kkc-at-002407643782 <kariny.dantas@hivecloud.com.br>
2020-11-15 21:53:46 -0300

    lists frontend

---
+++ frontend/src/app/services/contato.service.ts
@@ -0,0 +1,31 @@
+import { Contato } from './../models/contato.model';
+import { Fornecedor } from './../models/fornecedor.model';
+import { FormaPagamento } from './../models/formaPagamento.model';
+import { Categoria } from './../models/categoria.model';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {API_URL} from '../env';
+import { NumberFormatStyle } from '@angular/common';
+
+@Injectable({ providedIn: 'root' })
+export class ContatoService {
+
+  constructor(private http: HttpClient) {}
+
+  getById(id: number) {
+    return this.http.get(`${API_URL}/contato/${id}`);
+  }
+
+  putContato(contato: Contato, id: number) {
+    return this.http.put<Contato>(
+      `${API_URL}/contato/${id}`, contato);
+  }
+
+  deleteContato(id: number) {
+    return this.http.delete(`${API_URL}/contato/${id}`);
+  }
+
+  createContato(contato: Contato) {
+    return this.http.post<Contato>(`${API_URL}/contato/cadastro`, contato);
+  }
+}
